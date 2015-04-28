4d183eb68a25ad1035029d8960061e088b5c86b8
rbaqueros <rbaqueros@gmail.com>
2015-04-27 22:31:13 -0500

    Initial commit

---
+++ CountryInfo/Country.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Copyright (c) 2015 Rafael Baquero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CountryInfo
+{
+    public class Country : IComparable
+    {
+        public Country(string iso2Code, string countryName, BitmapImage countryFlag)
+        {
+            Iso2 = iso2Code;
+            Name = countryName;
+            Flag = countryFlag;
+
+        }
+
+        private string _iso2;
+
+        public string Iso2
+        {
+            get { return _iso2; }
+            set { _iso2 = value; }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private BitmapImage _flag;
+
+        public BitmapImage Flag
+        {
+            get { return _flag; }
+            set { _flag = value; }
+        }
+
+        public override string ToString()
+        {
+            return _name.ToString();
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Country otherCountry = obj as Country;
+            if(otherCountry==null)
+            {
+                throw new ArgumentException("Unable to compare. Object is not of type country");
+            }
+            return Name.CompareTo(otherCountry.Name);
+        }
+    }
+}
---
+++ CountryInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CountryInfo")]
+[assembly: AssemblyDescription("Country flags and names")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rafael Baquero")]
+[assembly: AssemblyProduct("CountryInfo")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfadf857-a4dc-48ae-adcd-6d78a0fa87ba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
